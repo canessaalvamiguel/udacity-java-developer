d9cfe07012dc640ad88159a1c533a2ed4fb45af4
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-05-17 23:47:30 -0500

    Add swagger library to document the API automatically

---
+++ Lesson7-documenting/src/main/java/com/udacity/document/DocumentApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*Go to http://localhost:8080/swagger-ui.html#/ to see the generated docs*/
+
+@SpringBootApplication
+public class DocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentApplication.class, args);
+	}
+
+}
