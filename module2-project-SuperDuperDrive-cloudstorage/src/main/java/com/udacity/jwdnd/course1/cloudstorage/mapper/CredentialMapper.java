9311570a9d86461c705fdc1fd129fa828a190a71
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-05-27 01:30:15 -0500

    SUPERDUPERDRIVE - Add CRUD support to credentials

--- module2-project-SuperDuperDrive-cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
+++ module2-project-SuperDuperDrive-cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -1,11 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
-import com.udacity.jwdnd.course1.cloudstorage.model.File;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -13,9 +9,18 @@ import java.util.List;
 public interface CredentialMapper {
 
     @Select("SELECT * FROM CREDENTIALS WHERE userid = #{userid}")
-    List<Credential> getCredentials(Integer userId);
+    List<Credential> getCredentials(Integer userid);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE credentialid = #{credentialid} AND userid = #{userid}")
+    Credential getCredential(Integer credentialid, Integer userid);
 
     @Insert("INSERT INTO CREDENTIALS(url, username, key, password, userid) VALUES (#{url}, #{username}, #{key}, #{password}, #{userid})")
     @Options(useGeneratedKeys = true, keyProperty = "credentialid")
     int insertCredential(Credential credential);
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialid = #{credentialid}")
+    int deleteCredential(int credentialid);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, key = #{key}, password = #{password} WHERE credentialid = #{credentialid}")
+    int updateCredential(Credential credential);
 }
--- module2-project-SuperDuperDrive-cloudstorage/src/main/resources/data.sql
+++ module2-project-SuperDuperDrive-cloudstorage/src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO FILES(filename, contenttype, filesize, userid) VALUES ('test01.pdf',
 INSERT INTO FILES(filename, contenttype, filesize, userid) VALUES ('test02.pdf', 'pdf', '4MB', 1);
 INSERT INTO NOTES(notetitle, notedescription, userid) VALUES ( 'My first note', 'This is a test note 01', 1 );
 INSERT INTO NOTES(notetitle, notedescription, userid) VALUES ( 'My second note', 'This is a test note 02', 1 );
-INSERT INTO CREDENTIALS(url, username, key, password, userid)  VALUES ( 'www.google.com', 'canessa01', 'key01', 'password01', 1 );
-INSERT INTO CREDENTIALS(url, username, key, password, userid)  VALUES ( 'www.facebook.com', 'canessa02', 'key02', 'password02', 1 );
+INSERT INTO CREDENTIALS(url, username, key, password, userid)  VALUES ( 'www.google.com', 'canessa01', 'bbUr2BtPpKV1uDGf1BYRlg==', '6yAiolaUihMYQbjLq8nJhg==', 1 );
+INSERT INTO CREDENTIALS(url, username, key, password, userid)  VALUES ( 'www.facebook.com', 'canessa02', 'bbUr2BtPpKV1uDGf1BYRlg==', '6yAiolaUihMYQbjLq8nJhg==', 1 );
